36311b0b6bb7668b6fde688fd7f52b94d13e82c4
marktoledoDEV <markalex.toledo1@gmail.com>
2018-12-09 23:19:26 -0800

    changed the State from an interface to a abstract class

--- yebois_framework/Assets/YeBoisFramework/Scripts/StateMachine/State.cs
+++ yebois_framework/Assets/YeBoisFramework/Scripts/StateMachine/State.cs
@@ -1,35 +1,28 @@
-﻿using System.Collections;
+﻿/*
+	Author: Mark Toledo
+	Description: An class that establishes the functions that a state could do in a Animator
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/*
-	Author: Mark Toledo
-	Description: An interface that establishes the functions that a state could do in a Animator
-*/
 namespace YeBoisFramework.StateMachine
 {
-	public class State : StateMachineBehaviour 
+	public abstract class State : StateMachineBehaviour 
 	{
 		#region Properties and Fields
-		public string stateName { get; set; }
-		public StateName stateEnum;
+		public virtual string stateName { get; set; }
 		#endregion
 
 		#region Unity Methods
 		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
 		{
-			Debug.Log("Now Entering State: " + stateEnum.ToString());
 		}
 		public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 		{
-			Debug.Log("Updating State: " + stateEnum.ToString());
 		}
 		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 		{
-			Debug.Log("Now Exiting State: " + stateEnum.ToString());
 		}
 		#endregion
 	}
-
-	public enum StateName { A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P}
 }
