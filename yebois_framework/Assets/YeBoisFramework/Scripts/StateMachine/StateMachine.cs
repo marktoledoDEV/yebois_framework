0de0d7c047a3cab00ab932b88869d546fd1c7f7e
marktoledoDEV <markalex.toledo1@gmail.com>
2018-12-09 23:20:09 -0800

    created the StateMachine Class

---
+++ yebois_framework/Assets/YeBoisFramework/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,36 @@
+﻿/*
+	Author: Mark Toledo
+	Description: Keeps states of its type consistent and in line
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace YeBoisFramework.StateMachine
+{
+	[RequireComponent(typeof(Animator))]
+	public abstract class StateMachine<tState> : MonoBehaviour where tState : State
+	{
+		#region Properties and Fields
+		protected Dictionary<string,tState> stateDictionary = new Dictionary<string, tState>();
+		#endregion
+
+		#region Components
+		private Animator smAnimator;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void Awake()
+		{
+			smAnimator = GetComponent<Animator>();
+			InitializeStates();
+		}
+		#endregion
+
+		#region Custom Methods
+		//Will bind any states from the animator together to make it easily accessible to the state machine
+		protected abstract void InitializeStates();
+		//Will be called in a statemachine controller to refresh the statemachine
+		protected abstract void RefreshStateMachine();
+		#endregion
+	}
+}
